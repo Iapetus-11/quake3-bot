d7bee3eed23aa5725e7a50403cbcda99f1e7a3bd
Milo Weinberg <iapetus011@gmail.com>
2022-10-16 12:03:01 -0400

    Add noqa: F401 comments for imports in models/__init__.py

--- bot/models/__init__.py
+++ bot/models/__init__.py
@@ -1,3 +1,3 @@
-from .base_model import BaseModel
-from .discord_guild import DiscordGuild
-from .quake3_server import Quake3Server
+from .base_model import BaseModel  # noqa: F401
+from .discord_guild import DiscordGuild  # noqa: F401
+from .quake3_server import Quake3Server  # noqa: F401
