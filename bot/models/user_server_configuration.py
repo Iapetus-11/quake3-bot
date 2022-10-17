16ad7e65d05ae818e324a13b068d6706d1be0530
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-10-17 12:43:19 +0000

    format / fix code

--- bot/models/user_server_configuration.py
+++ bot/models/user_server_configuration.py
@@ -1,10 +1,10 @@
-from .base_model import BaseModel
-
 from tortoise import fields
+
+from .base_model import BaseModel
 
 
 class UserQuake3ServerConfiguration(BaseModel):
-    server = fields.ForeignKeyField('models.Quake3Server')
-    discord_user = fields.ForeignKeyField('models.DiscordUser')
+    server = fields.ForeignKeyField("models.Quake3Server")
+    discord_user = fields.ForeignKeyField("models.DiscordUser")
 
     password = fields.CharField(max_length=128)
