ab3d160d1142aadb362f9b2e16a4345cfcdcfd64
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2022-10-20 23:05:03 +0000

    format / fix code

--- bot/utils/text.py
+++ bot/utils/text.py
@@ -1,5 +1,6 @@
 VALID_Q3_IDENT_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789-_."
 
+
 def truncate_string(string: str, to: int, *, ellipses: str = "…") -> str:
     if len(string) > to:
         return string[: to - len(ellipses)] + ellipses
